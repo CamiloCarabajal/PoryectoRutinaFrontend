882d0551092dd2eed674461d5cb5cd667c1f0f90
CamiloCarabajal <ultrakin3t@gmail.com>
2024-08-12 17:35:37 -0300

    First commit

---
+++ ProyectoRutinaFrontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Landing from './components/landing/Landing'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+  return (
+  <>
+   <Landing/> 
+  </>
+  )
+}
+
+export default App
---
+++ ProyectoRutinaFrontend/src/components/card/CardRuti.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardRuti = () => {
+  return (
+    <div>CardRuti</div>
+  )
+}
+
+export default CardRuti
