fb03d841080dbdb56b3ea042482eb523c8445254
JavierTonini <javiertonini97@gmail.com>
2024-08-28 18:05:02 -0300

    UnoFrontyBack

--- ProyectoRutinaFrontend/src/App.jsx
+++ ProyectoRutinaFrontend/src/App.jsx
@@ -1,37 +1,39 @@
-import React, {useState} from 'react';
-import RoutineForm from './components/routineForm/RoutineForm';
-import RoutineList from './components/routineList/RoutineList';
-import ExerciseList from './components/exerciseList/ExerciseList';
+import React, { useState } from "react";
+import RoutineForm from "./components/routineForm/RoutineForm";
+import RoutineList from "./components/routineList/RoutineList";
+import RoutineList2 from "./components/routineList2.jsx/RoutineList2";
+import ExerciseList from "./components/exerciseList/ExerciseList";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
- const [routines, setRoutines] = useState([]);
- const [selectedRoutine, setSelectedRoutine] = useState(null);
+  const [routines, setRoutines] = useState([]);
+  const [selectedRoutine, setSelectedRoutine] = useState(null);
 
-const addRoutine = (routine) =>{
-  setRoutines([...routines, routine]);
-};
-const updateRoutine = (updateRoutine) =>{
-  setRoutines(
-    routines.map((routine)=> routine.id === updateRoutine.id ? updateRoutine.id : routine)
-  );
-};
-const deleteRoutine = (routineId)=>{
-  setRoutines(
-    routines.filter((routine)=> routine.id !== routineId)
-  );
-}
+  const addRoutine = (routine) => {
+    setRoutines([...routines, routine]);
+  };
+  const updateRoutine = (updateRoutine) => {
+    setRoutines(
+      routines.map((routine) =>
+        routine.id === updateRoutine.id ? updateRoutine.id : routine
+      )
+    );
+  };
+  const deleteRoutine = (routineId) => {
+    setRoutines(routines.filter((routine) => routine.id !== routineId));
+  };
   return (
-    <div className='container'>
-    <h1> GYM : Rutinas de Entrenamiento</h1>
-    <RoutineList
-    routines={routines}
-    onEdit={setSelectedRoutine}
-    onDelete={updateRoutine}
-    />
-    <ExerciseList />
+    <div className="container">
+      <h1> GYM : Rutinas de Entrenamiento</h1>
+      <RoutineList
+        routines={routines}
+        onEdit={setSelectedRoutine}
+        onDelete={updateRoutine}
+      />
+      <RoutineList2 />
+      <ExerciseList />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
